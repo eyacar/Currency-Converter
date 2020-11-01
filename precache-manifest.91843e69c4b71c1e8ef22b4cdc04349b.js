e596ac3cd51c6c3f4d9ea4d810babbb2fecd8f4c
Ezequiel Yacar <ezequielyacar@Air-de-Ezequiel.fibertel.com.ar>
2020-11-01 17:44:24 -0300

    First Commit

---
+++ precache-manifest.91843e69c4b71c1e8ef22b4cdc04349b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b0bd7ff81ec3bed7416ec89216e7a69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a39aeb246a16b6c38b0a",
+    "url": "/static/css/main.749ea9d5.chunk.css"
+  },
+  {
+    "revision": "636f7154df3261f19cf1",
+    "url": "/static/js/2.babe9082.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.babe9082.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a39aeb246a16b6c38b0a",
+    "url": "/static/js/main.adb8e18b.chunk.js"
+  },
+  {
+    "revision": "c7222dfa689a79d95997",
+    "url": "/static/js/runtime-main.69b44adb.js"
+  }
+]);
